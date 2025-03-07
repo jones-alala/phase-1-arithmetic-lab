3757075f701f4ca13eed11cb7b5136b3ff986ba6
Johnson Alala <jonesalala95@gmail.com>
2025-03-07 23:31:40 +0300

    Completed the assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+const num1 = 32;
+const num2 = 2;
+const multiply = 62;
+
+//random number should be greater than zero
+const random = Math.floor(Math.random() * 100) + 1;
+
+//Remainder shuld be 4
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+// maximum number should be 20
+const max = Math.max(1, 5, 10, 15, 20);
